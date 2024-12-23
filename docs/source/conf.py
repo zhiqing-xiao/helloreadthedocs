ba1f8b0486ce1c268912c4ed7b189522012dc68a
zhiqing-xiao <96648982+zhiqing-xiao@users.noreply.github.com>
2024-12-24 01:36:38 +0800

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'myprojectname'
+copyright = '2025, MyAuthor'
+author = 'MyAuthor'
 
 release = '0.1'
 version = '0.1.0'
